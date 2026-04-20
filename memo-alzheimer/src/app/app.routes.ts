26005c7fd3ddcf6fbbdfe2abe45994e575bba7e6
Lawliet464 <lutherarthurhawkins@gmail.com>
2026-04-20 08:36:57 +0100

    ajout route

--- memo-alzheimer/src/app/app.routes.ts
+++ memo-alzheimer/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { StatistiquesComponent } from './statistiques/statistiques.component';
 import { Parametres } from './parametres/parametres';
 import { Jeu } from './jeu/jeu';
-
+import { Auth} from './features/auth/auth'
 
 export const routes: Routes = [
   { path: 'statistiques', component: StatistiquesComponent},
   { path: 'parametres', component: Parametres},
   { path: 'jeu', component: Jeu },
+  { path: 'auth', component: Auth},
   { path: '', redirectTo: 'statistiques', pathMatch: 'full' }
 ];
+
