07fbe9f86a9864c5f756a774fe79cdb7d05f309c
MAUVILLAIN Nina <nina.mauvillain@etu.univ-cotedazur.fr>
2026-04-26 23:34:00 +0200

    des trucs

--- front-end/src/app/core/services/jeu-communication.service.ts
+++ front-end/src/app/core/services/jeu-communication.service.ts
@@ -3,26 +3,27 @@ import { Subject } from 'rxjs';
 import { Theme, ElemTheme } from '../models/theme.model';
 import { THEMES } from '../../mocks/themes.mock';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class JeuService {
 
   themeChoisi: Theme | null = null;
-  elemsJeu: ElemTheme[]=[];
+  elemsJeu: ElemTheme[] = [];
   aideMemorisation: number | null = null;
   aideAssociation: number | null = null;
 
-  compteurErreurs: number = 0;
+  compteurErreursMemory: number = 0;
+  compteurErreursAssociation: number = 0;
+
   frequenceAide: number = 0;
-  tempsRetournement: number = 0;
+  temps: number = 0;
   role: 'accueilli' | 'aidant' | null = null;
 
   private finEtapeSource = new Subject<void>();
   finEtape$ = this.finEtapeSource.asObservable();
 
-  constructor() { }
+  constructor() {}
 
   setRole(r: 'accueilli' | 'aidant'): void {
     this.role = r;
@@ -32,25 +33,42 @@ export class JeuService {
     this.finEtapeSource.next();
   }
 
-  incrementErreur(): boolean {
-    this.compteurErreurs++;
-    if (this.compteurErreurs >= this.frequenceAide) {
-      this.compteurErreurs = 0;
+  incrementErreurMemory(): boolean {
+    this.compteurErreursMemory++;
+    if (this.compteurErreursMemory >= this.frequenceAide) {
+      this.compteurErreursMemory = 0;
+      return true;
+    }
+    return false;
+  }
+
+  incrementErreurAssociation(): boolean {
+    this.compteurErreursAssociation++;
+    if (this.compteurErreursAssociation >= this.frequenceAide) {
+      this.compteurErreursAssociation = 0;
       return true;
     }
     return false;
   }
 
   reset(): void {
-    this.compteurErreurs = 0;
+    this.compteurErreursMemory = 0;
+    this.compteurErreursAssociation = 0;
   }
 
-  configurerJeu(frequence: number, temps: number, paires: number, themeChoisi: Theme, aideMemorisation: number, aideAssociation: number): void {
+  configurerJeu(
+    frequence: number,
+    temps: number,
+    paires: number,
+    themeChoisi: Theme,
+    aideMemorisation: number,
+    aideAssociation: number
+  ): void {
     this.frequenceAide = frequence;
-    this.tempsRetournement = temps * 1000;
-    this.themeChoisi=themeChoisi;
-    this.aideMemorisation= aideMemorisation;
-    this.aideAssociation= aideAssociation;
+    this.temps = temps * 1000;
+    this.themeChoisi = themeChoisi;
+    this.aideMemorisation = aideMemorisation;
+    this.aideAssociation = aideAssociation;
     this.setElemsJeu(paires, themeChoisi);
   }
 
@@ -64,6 +82,7 @@ export class JeuService {
     if (this.elemsJeu.length < paires) {
       const autresElems: ElemTheme[] = [];
       const autresThemes = THEMES.filter(t => t.id !== theme.id);
+
       for (const autreTheme of autresThemes) {
         for (const e of autreTheme.elementsAssoc) {
           autresElems.push(e);
@@ -78,5 +97,4 @@ export class JeuService {
       }
     }
   }
-
 }
