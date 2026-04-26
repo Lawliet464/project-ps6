8568885987bcf818fd5c68c65868e11fee762772
Emma Videau <emma.videau@etu.unice.fr>
2026-04-26 22:28:25 +0200

    ajout thèmes

--- front-end/src/app/mocks/themes.mock.ts
+++ front-end/src/app/mocks/themes.mock.ts
@@ -6,8 +6,8 @@ export const THEMES: Theme[] = [
     nom: 'Se brosser les dents',
     type: 'texte+image',
     elementsAssoc: [
-      { id: 1, nom: 'Mettre du dentifrice', image: 'assets/images/toothbrush-toothpaste.png', themeId: 'Brossage' },
-      { id: 2, nom: 'Brosser les dents',    image: 'assets/images/action.png',   themeId: 'Brossage' },
+      { id: 1, nom: 'Mettre du dentifrice', image: 'assets/images/dentifrice_brosse.png', themeId: 'Brossage' },
+      { id: 2, nom: 'Brosser les dents',    image: 'assets/images/brosser_dents.png',   themeId: 'Brossage' },
       { id: 3, nom: 'Se rincer la bouche',  image: 'assets/images/rincer_bouche.png',     themeId: 'Brossage' },
     ]
   },
@@ -16,19 +16,39 @@ export const THEMES: Theme[] = [
     nom: 's\'habiller selon la météo',
     type: 'texte+image',
     elementsAssoc: [
-      { id: 1, nom: 'Regarder la météo',     image: 'assets/images/rain.png',     themeId: 'habillage' },
-      { id: 2, nom: 'Choisir ses vêtements', image: 'assets/images/raincoat.png', themeId: 'habillage' },
-      { id: 3, nom: 'S\'habiller',           image: 'assets/images/rainshoes.png', themeId: 'habillage' },
+      { id: 1, nom: 'Regarder la météo',     image: 'assets/images/pluie.png',     themeId: 'habillage' },
+      { id: 2, nom: 'Choisir ses vêtements', image: 'assets/images/vetement_pluie.png', themeId: 'habillage' },
+      { id: 3, nom: 'S\'habiller',           image: 'assets/images/s_habiller.png', themeId: 'habillage' },
     ]
   },
   {
-    id: 'hiver',
-    nom: 'jsp',
+    id: 'courses',
+    nom: 'Faire les courses',
     type: 'texte+image',
     elementsAssoc: [
-      { id: 1, nom: 'bonnet', image: 'assets/images/bonnet.png', themeId: 'hiver' },
-      { id: 2, nom: 'hat',   image: 'assets/images/hat.png',    themeId: 'hiver' },
-      { id: 3, nom: 'jacket',     image: 'assets/images/jacket.png',    themeId: 'hiver' },
+      { id: 1, nom: 'faire une liste', image: 'assets/images/liste.png', themeId: 'courses' },
+      { id: 2, nom: 'remplir le caddie',   image: 'assets/images/caddie.png',    themeId: 'courses' },
+      { id: 3, nom: 'aller à la caisse',     image: 'assets/images/caisse.png',    themeId: 'courses' },
+    ]
+  },
+  {
+    id: 'cuisiner',
+    nom: 'Faire la cuisine et la vaisselle',
+    type: 'texte+image',
+    elementsAssoc: [
+      { id: 1, nom: 'cuisiner', image: 'assets/images/cuisiner.png', themeId: 'cuisiner' },
+      { id: 2, nom: 'laver la vaisselle',   image: 'assets/images/laver_vaisselle.png',    themeId: 'cuisiner' },
+      { id: 3, nom: 'essuyer la vaisselle',     image: 'assets/images/essuyer_vaisselle.png',    themeId: 'cuisiner' },
+    ]
+  },
+  {
+    id: 'thé',
+    nom: 'Faire du thé',
+    type: 'texte+image',
+    elementsAssoc: [
+      { id: 1, nom: 'faire chauffer l\'eau', image: 'assets/images/bouilloire.png', themeId: 'thé' },
+      { id: 2, nom: 'mettre le sachet',   image: 'assets/images/sachet_tasse.png',    themeId: 'thé' },
+      { id: 3, nom: 'verser l\'eau',     image: 'assets/images/the.png',    themeId: 'thé' },
     ]
   }
 ];
